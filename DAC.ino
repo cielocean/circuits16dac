295973be90277b73e3044ec69171cace495dbcc7
cielocean <jeehyun.kim@students.olin.edu>
2016-04-30 20:41:51 -0400

    updated code

--- DAC.ino
+++ DAC.ino
@@ -1,97 +1,22 @@
-int sensorPin = A0;
-int sensorValue = 0;
+int Ain = A0;
+int value = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(3, LOW);digitalWrite(5, LOW);
-  digitalWrite(6, LOW);digitalWrite(9, LOW);
-  Serial.print("one");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, LOW);
-  digitalWrite(6, LOW);digitalWrite(9, HIGH);
-  Serial.print("two");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);digitalWrite(9, LOW);
-  Serial.print("three");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);digitalWrite(9, HIGH);
-  Serial.print("four");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);digitalWrite(9, LOW);
-  Serial.print("five");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);digitalWrite(9, HIGH);
-  Serial.print("fivesix");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);digitalWrite(9, LOW);
-  Serial.print("six");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, LOW);digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);digitalWrite(9, HIGH);
-  Serial.print("seven");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, LOW);
-  digitalWrite(6, LOW);digitalWrite(9, LOW);
-  Serial.print("eight");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, LOW);
-  digitalWrite(6, LOW);digitalWrite(9, HIGH);
-  Serial.print("nine");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);digitalWrite(9, LOW);
-  Serial.print("ten");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, LOW);
-  digitalWrite(6, HIGH);digitalWrite(9, HIGH);
-  Serial.print("eleven");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);digitalWrite(9, LOW);
-  Serial.print("twelve");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);digitalWrite(9, HIGH);
-  Serial.print("twelvethirteen");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);digitalWrite(9, LOW);
-  Serial.print("thirteen");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-  digitalWrite(3, HIGH);digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);digitalWrite(9, HIGH);
-  Serial.print("fourteen");
-  Serial.println(analogRead(sensorPin)* (5.0 / 1023.0));
-  delay(125);
-
+  for (byte i=0; i <=1111; i++){
+    digitalWrite(1,bitRead(i,0));
+    digitalWrite(2,bitRead(i,1));
+    digitalWrite(3,bitRead(i,2));
+    digitalWrite(4,bitRead(i,3));
+    Serial.println(analogRead(Ain)* (5.0 / 1023.0));
+  }
 }
